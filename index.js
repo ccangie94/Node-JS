bbe2ffdd413f4adcdddb38891cf92cfd276a4563
ccangie94 <ccangie94@gmail.com>
2022-04-26 19:38:27 -0500

    actualizacion de postman

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
-const express = require('express')
+const express = require('express');
+const { acceptsLanguages } = require('express/lib/request');
 const app = express()
 
+let id
+let name;
+let lastname;
+let age;
+
+
+app.use(express.json());
+
+app.post('/', function(request, response){
+  id= request.body.id
+  name = request.body.name
+  lastname = request.body.lastname
+  age = request.body.old
+  console.log(request.body);      // your JSON
+   response.send(request.body);    // echo the result back
+});
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World');
 })
 
-app.get('/Español', function (req, res) {
-    res.send('Hola mundo')
-  })
+app.get('/name', function (req, res) {
+  res.send(name);
+  console.log(name);
+})
 
-  app.get('/Portugues', function (req, res) {
-    res.send('Olá mundo')
-  })
+app.get('/lastname', function (req, res) {
+  res.send(lastname);
+})
+
+app.get('/age', function (req, res) {
+  res.send(27);
+})
 
 app.listen(3000)
